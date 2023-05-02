830f7c83dfdbec5aae1f7f267274b7908f8b126a
ZahraCassimjee <231131@virtualwindow.co.za>
2023-05-02 17:47:47 +0200

    js

---
+++ js/index.js
@@ -0,0 +1,48 @@
+//Exercise 1
+var firstName = "Zahra";
+let surname = "Cassimjee";
+const subject = "Development";
+const age = 19;
+
+console.log(firstName);
+console.log(surname);
+console.log(subject);
+console.log(age);
+
+let greeter = "Hello, my name is "+ firstName + " " + surname + ", I am "+ age + " years old and I am studying "+ subject; 
+ 
+console.log(greeter);
+
+//Exercise 2
+let numOne = 1;
+let numTwo = 2;
+let numThree = 4;
+let numFour = 10;
+let answer =0;
+
+numOne + numTwo / numThree * numFour;
+
+console.log(answer);
+
+//Exercise 3
+let timmy = 10;
+let susan = 25;
+
+
+
+//Exercise 4
+
+
+let john = 100;
+let tim = 100;
+let alfie = 100;
+let gwen = 100;
+let value = john + tim + alfie + gwen /4;
+
+let average = value / 100;
+
+console.log(average);
+
+updateMark = 7;
+
+console.log();
